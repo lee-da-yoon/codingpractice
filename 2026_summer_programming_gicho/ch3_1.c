d8c2c4ba555b78f55bab2fa81959fe4fc79816c1
lee-da-yoon <dlekdbs1121@gmail.com>
2026-06-25 12:42:45 +0900

    chapter3

---
+++ 2026_summer_programming_gicho/ch3_1.c
@@ -0,0 +1,7 @@
+# include <stdio.h>
+int main() {
+    puts("puts()는 한 줄에 문자열 출력함수"); // 한 줄 출력을 자동으로 
+    printf("printf()는 다양한 종류의");
+    puts("자바"); puts("C#"); // 프로그램의 이해에 도움이 된다면 한 줄에 여러 문장의 입력도 가능하다 
+    return 0;
+}
---
+++ 2026_summer_programming_gicho/ch3_실습예제_1.c
@@ -0,0 +1,15 @@
+/* 
+여러 줄에 걸친 주석 블럭 
+C 프로그램의 기초를 다지기 위한 주석, 문장, 키워드 등 이해
+*/
+
+# include <stdio.h>
+// 운영체제가 호출하는 함수, void로 매개변수 없음을 표시
+int main(void){
+    puts("3장 첫 C 프로그램!\n");
+    
+    printf("키워드 : int void return 등 \n");
+    printf("식별자 : main puts printf 등\n");
+
+    return 0;
+}
